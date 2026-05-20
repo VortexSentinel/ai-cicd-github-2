70c146555697514541b3318cbf0798a51d020a85
VortexSentineal <rajnish96jha@gmail.com>
2026-05-20 20:28:42 +0530

    Fix security issues

--- dangerous.py
+++ dangerous.py
@@ -1,7 +1,12 @@
+import os
+import shlex
 import subprocess
 
-def run_command(user_input):
-    """Run a shell command from user input."""
-    subprocess.call(user_input, shell=True)
 
-API_KEY = "sk-live-abc123def456"
+def run_command(user_input):
+    """Run a shell command safely."""
+    args = shlex.split(user_input)
+    subprocess.call(args, shell=False)
+
+
+API_KEY = os.environ.get("API_KEY")
